07667da1b582ca8b84618d13351c4af6ef3830c5
lesliemw <l.marie1598@gmail.com>
2025-01-05 01:49:27 +0000

    update files

---
+++ app/_components/Users/RegistrationForm.jsx
@@ -0,0 +1,141 @@
+"use client";
+
+import { useSignup } from "../../_api/useSignup";
+import Link from "next/link";
+
+import { useForm } from "react-hook-form";
+
+export default function RegistrationForm() {
+  const { signup, isLoading } = useSignup();
+  const { register, formState, handleSubmit } = useForm();
+  const { errors } = formState;
+
+  function onSubmit({ fname, lname, email, password }) {
+    signup({ fname, lname, email, password });
+  }
+
+  return (
+    <>
+      <div className="flex h-full flex-1 flex-col   justify-center  px-6 py-40 font-themeFont lg:px-8">
+        <div className="sm:mx-auto  sm:w-full sm:max-w-sm">
+          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign up for your new account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            <div>
+              <label
+                htmlFor="fname"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                First Name
+              </label>
+              <div className="mt-2">
+                <input
+                  error={errors?.fname?.message}
+                  id="fname"
+                  name="fname"
+                  type="text"
+                  disabled={isLoading}
+                  {...register("fname", { required: "This field is required" })}
+                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="lname"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  error={errors?.lname?.message}
+                  id="lname"
+                  name="lname"
+                  type="text"
+                  disabled={isLoading}
+                  {...register("lname", { required: "This field is required" })}
+                  className="block w-full rounded-md px-3 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  error={errors?.email?.message}
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  disabled={isLoading}
+                  {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: "Please provide a valid email address",
+                    },
+                  })}
+                  className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  error={errors?.password?.message}
+                  id="password"
+                  name="password"
+                  type="password"
+                  disabled={isLoading}
+                  {...register("password", {
+                    required: "This field is required",
+                    minLength: {
+                      value: 6,
+                      message: "Password must have a minimum of 6 characters.",
+                    },
+                  })}
+                  className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 disabled:cursor-not-allowed focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+              <p className="mt-2">
+                Already have an account?{" "}
+                <span className="text-indigo-400 hover:text-indigo-500">
+                  <Link href="/user/login">Sign in here</Link>
+                </span>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
